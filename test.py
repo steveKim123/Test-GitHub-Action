004d4afaef5d632ce9cee54d9608458904bc75c9
= <=>
2024-04-11 23:29:26 -0400

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+
+import uuid
+import calendar
+
+from .models import (
+    EntityType, Tag, Resource, ResourceTag,
+    Container, ContainerRelationship, Segment, Schema
+)
+import time
