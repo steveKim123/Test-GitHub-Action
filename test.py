4417931954a00e46a10649f98371e215261d359c
= <=>
2024-04-16 11:37:58 -0400

    puhs

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+from django.db import transaction
+
+from .models import ContainerRelationship
+
+
 class BaseDynamicQuery(object):
     def set_children_by_query(self, parent_id, start_order):
         with transaction.atomic():
             child_relations = ContainerRelationship.objects.filter(id=parent_id)
+            print("child_relations", child_relations)
