ffbe6b25806618379db26e4f48bf59961d3a67ae
= <=>
2024-04-16 11:33:31 -0400

    to

--- test.py
+++ test.py
@@ -2,4 +2,3 @@ class BaseDynamicQuery(object):
     def set_children_by_query(self, parent_id, start_order):
         with transaction.atomic():
             child_relations = ContainerRelationship.objects.filter(id=parent_id)
-
